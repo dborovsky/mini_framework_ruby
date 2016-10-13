dd70d90d13ab1e7cb944b1c7e4b830dc3061eff7
Dmitriy Borowskiy <d.borowskij@gmail.com>
2016-10-13 22:54:42 +0300

    routing

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-ruby '2.3.0'
+ruby '2.3.1'
 gem 'rack'
+gem 'puma'
--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
+require 'yaml'
+ROUTES = YAML.load(File.read(File.join(File.dirname(_FILE_), "app", "routes.yml")))
+
+require ".lib/router"
+
 class App
+  attr_reader :router
+
+  def initialize
+    @router = Router.new(ROUTES)
+  end  
+
   def call(env)
-    [200, {}, ['Hello, world']]
+    result = router.resolve(env)
+    [result.status, result.headers, result.content]
   end
 end
